43a40d48d25f087e41c81ff80360d6b04a2f4b71
Vivek <viveknagre198@gmail.com>
2021-08-24 23:11:44 +0530

    console |

--- creation.java
+++ creation.java
@@ -1,33 +1,32 @@
-import java.io.File;  
+import java.io.File;
 import java.io.IOException;
+
 public class creation {
-public static void main(String[] args) {
-    File file=new File("./vivek.txt");
-    if (file.isFile()) {
-        System.out.println("file alredy presnt");
+    public static void main(String[] args) {
+        File file = new File("./vivek.txt");
+        if (file.isFile()) {
+            System.out.println("file alredy presnt");
+        } else {
+            System.out.println("file not found");
+
+        }
+
+        try {
+            boolean result = file.createNewFile();
+
+            System.out.println("file is  :" + file.getName());
+
+            // basic method in file give imporatanat information
+            // System.out.println("file is :"+file.getCanonicalPath());
+            // System.out.println("file is readeable:"+file.canRead());
+            // System.out.println("file is writble:"+file.canWrite());
+            // System.out.println("file is exixt:"+file.exists());
+            // System.out.println("file is exicutable:"+file.canExecute());
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
-    else{
-        System.out.println("file not found");
-        
-    }
-    
 
-    try {
-        boolean result=file.createNewFile();
-
-        System.out.println("file is  :"+file.getName());
-
-        // basic  method in file give imporatanat information 
-        // System.out.println("file is  :"+file.getCanonicalPath());
-        // System.out.println("file is readeable:"+file.canRead());
-        // System.out.println("file is writble:"+file.canWrite());
-        // System.out.println("file is exixt:"+file.exists());
-        // System.out.println("file is exicutable:"+file.canExecute());
-
-    } catch (IOException e) {
-        System.out.println(e);
-    }
- 
-}
-    
 }
