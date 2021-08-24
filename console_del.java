0d68aefc16d5bb44f2793ccc4ca8b3510f67f817
Vivek <viveknagre198@gmail.com>
2021-08-24 23:21:03 +0530

    console based delimeter

--- console_del.java
+++ console_del.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class console_del {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+         String name = sc.next();
+         int age = sc.nextInt();
+         String city = sc.next();
+        
         System.out.print("name\t|\tage\t|\tcity\t|");
         // System.out.println("_______________________________________");
         System.out.println("\n---------------------------------------");
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 1; i++) {
 
-            String name = sc.next();
-            System.out.print("\t" + name + "\t|");
+            // String name = sc.next();
+            // int age = sc.nextInt();
+            // String city = sc.next();
+            
+            System.out.print(name + "\t|");
 
-            int age = sc.nextInt();
             System.out.print("\t" + age + "\t|");
 
-            String city = sc.nextLine();
             System.out.print("\t" + city + "\t|");
 
         }
