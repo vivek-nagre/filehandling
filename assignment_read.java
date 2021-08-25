9cbdb33cdb8f2d284d2e3d6e54d8bcc3373427f8
Vivek <viveknagre198@gmail.com>
2021-08-25 22:52:50 +0530

    record fetching comoplited

--- assignment_read.java
+++ assignment_read.java
@@ -1,10 +1,14 @@
 import java.io.*;
+import java.util.Scanner;
 import java.io.IOException;
+import java.nio.file.Files;
+
+import java.nio.file.Paths;
 import java.io.FileReader;
 public class assignment_read {
 public static void main(String[] args) {
     File file=new File("./vivek.txt");
-
+    Scanner sc=new Scanner(System.in);
     try {
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
@@ -14,7 +18,16 @@ public static void main(String[] args) {
         {
             System.out.println(st);
             
+            
         }
+        System.out.println();
+        System.out.println();
+        System.out.print("enter the record you want to change:");
+        int update=sc.nextInt();    
+
+        String fileNo=Files.readAllLines(Paths.get("./vivek.txt")).get(update+1);
+            System.out.println(fileNo);
+        
     } catch (IOException e) {
         System.out.println(e);
     }
